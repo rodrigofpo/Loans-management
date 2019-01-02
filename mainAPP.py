1f0f4026f39a1040ea8c1a0343f2c392f75edc45
cdgato <43214946+cdgato@users.noreply.github.com>
2019-01-02 01:03:01 -0200

    Arquivo main

---
+++ mainAPP.py
@@ -0,0 +1,23 @@
+'''
+Projeto de software - Tecnicas de programacao II
+
+1- Descriçao do projeto : descrever o que o seu projeto faz
+
+
+2- Data: data em que o projeto foi feito
+
+
+3- Equipe: listar o nome - email - matricula dos alunos
+- Fulano de tal  - Fulano@gmail.com - 201800123
+
+
+4 -Modulos que compoe o software: descrever a funçao de cada arquivo
+adicional que compoe o software
+
+- arquivos.py : Esse modulo fornece as funções para salvar e ler
+os dados da aplicação em arquivo usando a biblioteca picles
+
+'''
+
+
+#aqui começa o software ...
