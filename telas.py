8fa37f03deaf81c26e7075d09574b5ed9d740294
Rômulo Sá <romulo.sa153@live.com>
2019-01-18 13:51:25 -0300

    Cadastro
    Minha tela e a atualização do meu mod de cadastro.

---
+++ telas.py
@@ -0,0 +1,23 @@
+from mod_dados import *
+import PySimpleGUI as sg
+
+
+def tela_cadastro():
+    layout = [
+                [sg.Text("Cadastrar emprestimo")],
+                [sg.Text("Nome", size=(15, 1)), sg.InputText()],
+                [sg.Text("Telefone Fixo", size=(15, 1)), sg.InputText()],
+                [sg.Text("Celular", size=(15,1)), sg.InputText()],
+                [sg.Text("E-mail", size=(15,1)), sg.InputText()],
+                [sg.Text("De onde conhece", size=(15,1)), sg.InputText()],
+                [sg.Text("Data", size=(15,1)), sg.InputText("ex: dia/mês/ano", do_not_clear=False)],
+                [sg.Text("Item", size=(15,1)), sg.InputText()],
+                [sg.Button("Cadastrar",button_color=('white', 'springgreen4')),
+                 sg.Cancel(button_text="Cancelar",button_color=('white', 'firebrick3'))]
+                ]
+
+    janela = sg.Window("Teste").Layout(layout)
+    botao, valores = janela.Read()
+
+    if botao == 'Cadastrar':
+        cadastrar(valores)
