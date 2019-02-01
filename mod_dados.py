8a7acc9120480c21526161148b45e28e1a154fdd
Rômulo Sá <romulo.sa153@live.com>
2019-02-01 15:55:07 -0300

    Mod
    Atualização na tela e na função cadastro, adicionado tratamento de erro.

--- mod_dados.py
+++ mod_dados.py
@@ -1,30 +1,34 @@
 import pickle
 from datetime import datetime
-
+import PySimpleGUI as sg
 try:
     with open('dadosEmprestimos.bin', 'rb') as ler:
         emprestimos = pickle.load(ler)
 except Exception:
     emprestimos = []
 
-
+"""
+A função 'cadastrar' vai receber os inputs do arquivo tela, da função 'tela_cadastrar' e adicionar seus valores em um
+dicionário, e depois adicionar esse dicionário à lista global de emprestimo
+"""
 def cadastrar(informacoes):
-
-    nome = informacoes[0]
-    telefone = informacoes[1]
-    celular = informacoes[2]
-    email = informacoes[3]
-    vivencia = informacoes[4]
-    data = informacoes[5]
-    item = informacoes[6]
-    data = datetime.strptime(data, '%d/%m/%Y')
-
-    emprestimo = {'nome': nome, 'telefone': telefone, 'celular': celular, 'email': email, 'vivencia': vivencia,
-                  'item': item, 'data': data}
-
-    emprestimos.append(emprestimo)
-    gravar_dados()
-    print("Cadastro Salvo!")
+    try:
+        nome = informacoes[0]
+        telefone = informacoes[1]
+        celular = informacoes[2]
+        email = informacoes[3]
+        vivencia = informacoes[4]
+        data = informacoes[5]
+        item = informacoes[6]
+        data = datetime.strptime(data, '%d/%m/%Y').date()
+        data = datetime.today().date()
+        emprestimo = {'nome': nome, 'telefone': telefone, 'celular': celular, 'email': email, 'vivencia': vivencia,
+                      'item': item, 'data': data}
+        emprestimos.append(emprestimo)
+        gravar_dados()
+        sg.Popup("Cadastro realizado com sucesso!", button_color=('white', 'springgreen4'))
+    except ValueError:#Tratamendo de erro, caso o usuário informe a data em um formato não aceito
+        sg.Popup("Falha no cadastro!", button_color=('white', 'springgreen4'))
 
 def gravar_dados():
     with open('dadosEmprestimos.bin', 'wb') as fgravar:
--- telas.py
+++ telas.py
@@ -1,23 +1,26 @@
 from mod_dados import *
 import PySimpleGUI as sg
 
-
+"""A função 'tela_cadastro' vai receber as informações do usuário"""
 def tela_cadastro():
     layout = [
                 [sg.Text("Cadastrar emprestimo")],
                 [sg.Text("Nome", size=(15, 1)), sg.InputText()],
                 [sg.Text("Telefone Fixo", size=(15, 1)), sg.InputText()],
-                [sg.Text("Celular", size=(15,1)), sg.InputText()],
-                [sg.Text("E-mail", size=(15,1)), sg.InputText()],
-                [sg.Text("De onde conhece", size=(15,1)), sg.InputText()],
-                [sg.Text("Data", size=(15,1)), sg.InputText("ex: dia/mês/ano", do_not_clear=False)],
-                [sg.Text("Item", size=(15,1)), sg.InputText()],
-                [sg.Button("Cadastrar",button_color=('white', 'springgreen4')),
-                 sg.Cancel(button_text="Cancelar",button_color=('white', 'firebrick3'))]
+                [sg.Text("Celular", size=(15, 1)), sg.InputText()],
+                [sg.Text("E-mail", size=(15, 1)), sg.InputText()],
+                [sg.Text("De onde conhece", size=(15, 1)), sg.InputText()],
+                [sg.Text("Data", size=(15, 1)), sg.InputText("ex: dia/mês/ano", do_not_clear=False)],
+                [sg.Text("Item", size=(15, 1)), sg.InputText()],
+                [sg.Button("Cadastrar", button_color=('white', 'springgreen4')),
+                 sg.Button("Cancelar", button_color=('white', 'firebrick3'))]
                 ]
 
     janela = sg.Window("Teste").Layout(layout)
     botao, valores = janela.Read()
 
     if botao == 'Cadastrar':
-        cadastrar(valores)
+        cadastrar(valores)#aqui os dados são repassadas para a função 'cadastrar' e adicionadas à lista global de emprestimos
+
+    elif botao == 'Cancelar':
+        janela.Close()
